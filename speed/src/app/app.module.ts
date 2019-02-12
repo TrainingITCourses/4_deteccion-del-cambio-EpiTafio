a26250d8c4bb0ba8a5337b938a805bc2e76365e6
Kepa I. González <kepagrg@gmail.com>
2019-02-12 13:49:28 +0100

    Revert "Segunda revision"
    This reverts commit 40a9a760fbe4cddbb5564445f76acac1c0b1dd7e.

--- speed/src/app/app.module.ts
+++ speed/src/app/app.module.ts
@@ -3,16 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SalidaComponent } from './salida/salida.component';
-import { BuscadorComponent } from './buscador/buscador.component';
-import { ListaLanzamientosComponent } from './lista-lanzamientos/lista-lanzamientos.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SalidaComponent,
-    BuscadorComponent,
-    ListaLanzamientosComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
